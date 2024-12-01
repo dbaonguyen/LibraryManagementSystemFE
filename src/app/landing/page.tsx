fc72e3f68784733c75ca941cb0d990c7a8a13e18
dbaonguyen <baond39@gmail.com>
2024-12-01 12:19:46 +0700

    first commit

---
+++ src/app/landing/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <Button>Click me</Button>
+      
+    </div>
+  )
+}
+
+export default About
